766fabc839479d00d1f128c83bf77efe7acc4693
AlMarina <85302136+AlMarina@users.noreply.github.com>
2023-11-30 22:45:13 +0000

    Deploying to gh-pages from @ AlMarina/goit-react-hw-05-movies@fe6a7f86c5263c73e1749e069c1df8146afa9267 🚀

---
+++ static/js/856.551dac2b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[856],{856:function(h,v,t){t.d(v,{owp:function(){return e}});var a=t(983);function e(h){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M18 3v2h-2V3H8v2H6V3H4v18h2v-2h2v2h8v-2h2v2h2V3h-2zM8 17H6v-2h2v2zm0-4H6v-2h2v2zm0-4H6V7h2v2zm10 8h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2V7h2v2z"}}]})(h)}}}]);
+//# sourceMappingURL=856.551dac2b.chunk.js.map
